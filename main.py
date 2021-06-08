8f3a102f6330810c9cfd98dd42d344a8acd596e1
rajkhare05 <06rajkhare04@gmail.com>
2021-06-08 13:46:14 +0530

    msgStatusAndmsgId

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+import asyncio
 import time
+
+YOU = "//span[@aria-label = 'You:']"
+WHATSAPP_WEB_LINK = 'https://web.whatsapp.com/'
 
 class whatsApp:
 	def __init__(self, headless = False):
@@ -18,27 +22,31 @@ class whatsApp:
 
 		# assuming the QR Code is already scanned
 		# and "keep me signed in" is enabled (possibility)
-		self.driver.get('https://web.whatsapp.com/')
+		self.driver.get(WHATSAPP_WEB_LINK)
 
 		time.sleep(3) # to scan QR Code during this time
 		self.message_bar = self.driver.find_element_by_xpath("//div[@class = '_2A8P4']")
-		self.last_msg_time = None
+		self.last_msg_id = self.get_last_text_msg_id()
 
-	def is_connected():
+	def is_connected(self):
 		if self.driver.title == 'WhatsApp':
 			return True
 		return False
 
-	def quit_driver():
+	def quit_driver(self):
 		self.driver.quit()
 	
-	def send_message(message: str):
+	def send_message(self, message: str):
 		'''
 			send a text message
 		'''
 		self.message_bar.send_keys(message + Keys.RETURN)
+		# time.sleep(1.8)
+		# if self.message_sent():
+		# 	return True
+		# return False
 	
-	def send_file(file: str):
+	def send_file(self, file: str):
 		attach_icon = self.driver.find_element_by_xpath("//span[@data-icon = 'clip']")
 		attach_icon.click()
 		media_file = self.driver.find_element_by_xpath("//span[@data-icon = 'attach-image']")
@@ -50,9 +58,30 @@ class whatsApp:
 		media_send_button.click()
 		# self.last_msg_time = time.localtime().tm_min
 
-	def message_sent():
+	def message_sent(self):
 		'''
-			check if text message is delivered or not 
+			if text message is sent or not 
 		'''
-		if self.driver.find_elements_by_xpath("//span[@dir='ltr']//span"):
-			pass
+		if self.driver.find_elements_by_xpath(YOU+"//..//span[@aria-label = ' Sent ']"):
+			return True
+		return False
+	
+	def message_delivery(self):
+		if driver.find_elements_by_xpath(YOU+"//..//span[aria-label = ' Delivered ']"):
+			return True
+		return False
+
+	def message_read(self):
+		'''
+			if text message is seen by participent (or everyone: in group)
+		'''
+		if driver.find_elements_by_xpath(YOU+"//..//span[aria-label = ' Read ']"):
+			return True
+		return False
+
+	def get_last_text_msg_id(self):
+		'''
+			returns the last message data-id
+		'''
+		id = driver.find_elements_by_xpath("div[@tabindex='-1']")[-3].get_attribute('data-id')
+		return id
